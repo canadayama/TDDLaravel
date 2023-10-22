24f5f9ffb94ea14b0fd6c9a41304f6899002c3b9
Richard Y. Hayashi <rhayashi@gruuw.com>
2023-10-22 21:31:18 +0900

    This commit adds the book resource.

---
+++ library/app/Http/Controllers/BooksController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    /**
+     * 
+     */
+    protected function validateRequest()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'author' => 'required',
+        ]);
+    }
+
+    /**
+     * 
+     */
+    public function store()
+    {
+        Book::create($this->validateRequest());
+    }
+
+    /**
+     * 
+     */
+    public function update(Book $book)
+    {
+        $book->update($this->validateRequest());
+    }
+}
